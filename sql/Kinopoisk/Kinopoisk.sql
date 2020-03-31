51c2c6a0aba3c443a07b1e2be793a8b1f3aaa9cd
alex-kalinichenko <alex@kalinichenko.info>
2020-03-31 12:16:34 +0300

    Kinopoisk

--- sql/Kinopoisk/Kinopoisk.sql
+++ sql/Kinopoisk/Kinopoisk.sql
@@ -1,5 +1,12 @@
-SHOW DATABASES;
+-- Данная база данных преднаначена для хранения информации о фильмах (как документальных,
+-- так и художественных), актёрах, режисёрах, сценаристах и все[ остальных людей,
+-- задействованных в съёмках. Также в БД храниться информация о пользователях системы,
+-- реализована система хранения отзывов и оценок фильмов. Рассчитываются рейтинги фильмов.
+-- БД организована по принципу подобия сайта kinopoisk.ru
 
+
+SHOW DATABASES;
+DROP DATABASE IF EXISTS kinopoisk;
 CREATE DATABASE kinopoisk;
 USE kinopoisk;
 SHOW TABLES;
@@ -17,7 +24,8 @@ CREATE TABLE movie (
  grosses_usa INT UNSIGNED,
  grossses_all INT UNSIGNED,
  first_performance DATE,
- firts_prformace_russia DATE
+ firts_prformace_russia DATE,
+ tv_show SET('0', '1')
 );
 DESC movie;
 SELECT * FROM movie;
@@ -57,7 +65,8 @@ CREATE TABLE staff (
  director_id INT UNSIGNED,
  composer_id INT UNSIGNED,
  art_director_id INT UNSIGNED,
- editor_id INT UNSIGNED
+ editor_id INT UNSIGNED,
+ cameraman_id INT UNSIGNED
 );
 DESC staff;
 SELECT * FROM staff;
@@ -98,3 +107,54 @@ CREATE TABLE rating (
 );
 DESC rating;
 SELECT * FROM rating;
+
+-- 8. TV show & episods
+DROP TABLE IF EXISTS tv_show;
+CREATE TABLE tv_show (
+ movie_id INT UNSIGNED NOT NULL PRIMARY KEY,
+ number_seasons INT UNSIGNED,
+ number_episods INT UNSIGNED
+);
+DESC tv_show;
+SELECT * FROM tv_show;
+
+
+-- 9. Sumilar movies. Table for recommendations
+DROP TABLE IF EXISTS movie_like;
+CREATE TABLE movie_like (
+ movie_id_1 INT UNSIGNED NOT NULL PRIMARY KEY,
+ movie_id_2 INT UNSIGNED NOT NULL
+);
+DESC movie_like;
+SELECT * FROM movie_like;
+
+-- 10. Media content & links
+DROP TABLE IF EXISTS media;
+CREATE TABLE media (
+ movie_id INT UNSIGNED NOT NULL PRIMARY KEY,
+ poster_link VARCHAR(255),
+ trailer_link VARCHAR(255),
+ picture_link VARCHAR(255)
+);
+
+-- 11. Awards list
+DROP TABLE IF EXISTS award_list;
+CREATE TABLE award_list (
+ award_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ award_name VARCHAR(255)
+);
+DESC award_list;
+SELECT * FROM award_list;
+
+
+-- 12. Awarded movies
+DROP TABLE IF EXISTS awards;
+CREATE TABLE awards (
+ movie_id INT UNSIGNED NOT NULL PRIMARY KEY,
+ award_id INT UNSIGNED
+);
+DESC awards;
+SELECT * FROM awards;
+
+
+
