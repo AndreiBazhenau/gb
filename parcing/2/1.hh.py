7a65ed88f1a804a5a5a2b38bf28eef2ab86ec373
alex-kalinichenko <alex@kalinichenko.info>
2020-06-09 04:07:20 +0300

    Parcing hw 2: 90%

--- parcing/2/1.hh.py
+++ parcing/2/1.hh.py
@@ -1,21 +1,51 @@
 from bs4 import BeautifulSoup as bs
 import requests
-from pprint import pprint
+import pandas as pd
 
 # https://spb.hh.ru/search/vacancy?area=1&st=searchVacancy&text=data+scientist&fromSearch=true
-link = 'https://spb.hh.ru/search/vacancy?'
-headers = {'User-Agent': 'api-test-agent', 'Accept': '*/*'}
+site = 'https://spb.hh.ru/search/vacancy?'
 area = 'area=2'
-text = 'text=data+scientist'
+search = 'data+scientist'  # data+scientist  python
+headers = {'User-Agent': 'api-test-agent', 'Accept': '*/*'}
 
+link = site + area + '&st=searchVacancy&text=' + search + '&fromSearch=true'
+# <a class="bloko-link HH-LinkModifier" data-position="0" data-qa="vacancy-serp__vacancy-title"
+# data-requestid="1591655510397b99f8b5c9ac3c44ccd6" data-totalvacancies="54"
+# href="https://spb.hh.ru/vacancy/37369353?query=data%20scientist" target="_blank">Data Scientist</a>
+
+next_page = '#'
+df = pd.DataFrame(columns=['Vacancy', 'Salary', 'Company', 'Link'])
+i = 1
 try:
-    response = requests.get(link + area + '&st=searchVacancy&' + text + '&fromSearch=true', headers=headers, timeout=5)
+    while next_page:
+        response = requests.get(link, headers=headers, timeout=5).text
+        soup = bs(response, 'lxml')
+        # print(soup.title.string)
 
-    soup = bs(response.text, 'lxml')
+        for tag in soup.find_all('div', {'class': 'vacancy-serp-item'}):
+            # print(tag)
+            vacancy = tag.find('a', {'class': 'bloko-link HH-LinkModifier'})
+            # print(f"{i}: {vacancy.get_text()} : {vacancy['href']}")
+            salary = tag.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'})
+            # if salary:
+            #     print(salary.get_text())
+            # else:
+            #     print("NA")
+            company = tag.find('a', {'data-qa': 'vacancy-serp__vacancy-employer'})
+            # print(company)
 
-    #elem = soup.find_all(attrs={'class': 'bloko-link HH-LinkModifier'})
-    elem = soup.find_all('a', {'class': 'bloko-link HH-LinkModifier'})
-    pprint(elem)
+            df.loc[i] = [vacancy.get_text(), salary, company.get_text(strip=True), vacancy['href']]
+            i += 1
+            salary = ''
+
+        next_page_link = soup.find('a', {'class': 'bloko-button HH-Pager-Controls-Next HH-Pager-Control'})
+
+        if next_page_link:
+            next_page = next_page_link['href']
+        else:
+            next_page = ''
+            # print('No more vacancies')
+        link = 'https://spb.hh.ru' + next_page
 
 except requests.exceptions.ConnectTimeout:
     print('Connection timeout')
@@ -27,3 +57,5 @@ except requests.exceptions.ConnectionError:
     print('Connection error')
 except requests.exceptions.RequestException:
     print('General Error')
+
+print(df.to_string())
