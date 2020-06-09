c8f89f0249679b370e3a0cc490ba4b4f27629321
alex-kalinichenko <alex@kalinichenko.info>
2020-06-09 17:31:21 +0300

    HW 2 99.9% done

--- parcing/2/1.hh.py
+++ parcing/2/1.hh.py
@@ -2,15 +2,54 @@ from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
 
+
+def get_salary(s):
+    def get_currency(s):
+        s_len = len(s)
+        cur = ''
+        i = 0
+        while i < s_len:
+            a = s[i]
+            if '0' <= a <= '9':
+                i += 1
+            else:
+                cur = cur + a
+                i += 1
+        return cur
+
+    if s.startswith('от'):
+        s = s.replace('от', '')
+        cur = get_currency(s)
+        s_min = s.replace(cur, '')
+        s_max = 'NA'
+    elif s.startswith('до'):
+        s = s.replace('до', '')
+        cur = get_currency(s)
+        s_max = s.replace(cur, '')
+        s_min = 'NA'
+    elif s.find('-') >= 5:
+        pos = s.find('-')
+        s_min = s[:pos]
+        s_max = s[pos + 1:]
+        cur = get_currency(s_max)
+        s_max = s_max.replace(cur, '')
+    else:
+        s_min = 'NA'
+        s_max = 'NA'
+        cur = 'NA'
+
+    return s_min, s_max, cur
+
+
 site = 'https://spb.hh.ru/search/vacancy?'
 area = 'area=2'
-search = 'data+scientist'  # data+scientist  python
+search = 'python'  # data+scientist  python
 headers = {'User-Agent': 'api-test-agent', 'Accept': '*/*'}
 
 link = site + area + '&st=searchVacancy&text=' + search + '&fromSearch=true'
 
 next_page = '#'
-df = pd.DataFrame(columns=['Site', 'Vacancy', 'Salary', 'Company', 'Link'])
+df = pd.DataFrame(columns=['Site', 'Vacancy', 'Min salary', 'Max Salary', 'Currency', 'Company', 'Link'])
 i = 1
 try:
     while next_page:
@@ -21,22 +60,27 @@ try:
         for tag in soup.find_all('div', {'class': 'vacancy-serp-item'}):
             # print(tag)
             vacancy = tag.find('a', {'class': 'bloko-link HH-LinkModifier'})
-            # print(f"{i}: {vacancy.get_text()} : {vacancy['href']}")
+            print(f"{i}: {vacancy.get_text()} : {vacancy['href']}")
             salary = tag.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'})
             if salary:
                 # print(salary.get_text())
                 salary = salary.get_text().replace(" ", "").replace(" ", "")
+                s_min, s_max, cur = get_salary(salary)
+                print(s_min, s_max, cur)
+            else:
+                s_min, s_max, cur = 'NA', 'NA', 'NA'
+
                 # print(salary)
-                if salary.startswith('от'):
-                    salary = salary.replace("от", "")
-                    salary_min = salary
-            # else:
-                #print("NA")
+            #     if salary.startswith('от'):
+            #         salary = salary.replace("от", "")
+            #         salary_min = salary
+            # # else:
+            #     #print("NA")
 
             company = tag.find('a', {'data-qa': 'vacancy-serp__vacancy-employer'})
-            # print(company)
+            print(company)
 
-            df.loc[i] = ['hh.ru', vacancy.get_text(), salary, company.get_text(strip=True), vacancy['href']]
+            df.loc[i] = ['hh.ru', vacancy.get_text(), s_min, s_max, cur, company.get_text(strip=True), vacancy['href']]
             i += 1
             salary = ''
 
@@ -44,6 +88,7 @@ try:
 
         if next_page_link:
             next_page = next_page_link['href']
+            print(next_page_link['href'])
         else:
             next_page = ''
             # print('No more vacancies')
@@ -62,5 +107,3 @@ except requests.exceptions.RequestException:
 
 print(df.to_string())
 df.to_csv(f'hh_{search}.csv', index=False, encoding='utf-8')
-print('Export done!')
-
