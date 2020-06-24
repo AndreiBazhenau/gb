0b41bbdc704c408e9b1ef26e5a6de2f881e5f01a
alex-kalinichenko <alex@kalinichenko.info>
2020-06-24 16:16:44 +0300

    Parcing HW 5 Selenum done

--- parcing/5.selenium/2.mvideo_parcer.py
+++ parcing/5.selenium/2.mvideo_parcer.py
@@ -3,34 +3,48 @@
 
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+import json
+from pprint import pprint
 import time
+from pymongo import MongoClient
 
-driver = webdriver.Chrome('../../../selenium/chromedriver.exe')
+chrome_options = Options()
+chrome_options.add_argument('start-maximized')
+
+driver = webdriver.Chrome('../../../selenium/chromedriver.exe', options=chrome_options)
 driver.get('https://www.mvideo.ru')
 assert 'М.Видео - интернет-магазин цифровой и бытовой техники и электроники,' \
        ' низкие цены, большой каталог, отзывы. - Москва' in driver.title
 
-time.sleep(5)
-elem1 = driver.find_element_by_class_name('c-product-tile').text #.get_property('data-product-info')
-#print(elem1)
+wait = WebDriverWait(driver, 5).until(ec.element_to_be_clickable((By.XPATH, "//a[@class='sel-product-tile-title']")))
 
-# blocks = driver.find_element('gtm-push-products')
-blocks = WebDriverWait(driver, 5).until(ec.element_to_be_clickable((By.XPATH, "(//div[@class='gallery-layout sel-hits-block '])[2]"))).text
-print(blocks)
+# сначала скролим до конца т.к. при каждом нажатии подгружаются новые товары, а старые не удалются
+# таким образом избегаем дублей
+time.sleep(3)
+while True:
+    try:
+        next_button = driver.find_element_by_xpath(
+            "(//div[@class='gallery-layout sel-hits-block '])[2]//a[@class='next-btn sel-hits-button-next']")
+        actions = ActionChains(driver)
+        actions.move_to_element(next_button).click(next_button).perform()
+    except:
+        break
 
-print(" ====== ")
+# собираем все товары
+hits_block = driver.find_elements_by_xpath(
+    "(//div[@class='gallery-layout sel-hits-block '])[2]//a[@class='sel-product-tile-title']")
+items = []
+for item in hits_block:
+    item = json.loads(item.get_attribute('data-product-info'))
+    items.append(item)
+    pprint(item)
 
-block = WebDriverWait(driver, 5).until(ec.element_to_be_clickable((By.XPATH, "(//div[@class='gallery-layout sel-hits-block '])[2]"))).get_attribute('data-init-param')
-print(block)
-
-print(" ====== ")
-#block15 = WebDriverWait(driver, 5).until(ec.element_to_be_clickable((By.XPATH, "((//div[@class='gallery-layout sel-hits-block '])[2]//ul//@data-product-info")))
-
-block1 = driver.find_elements_by_xpath("//div[@class='gallery-layout sel-hits-block ']//ul//@data-product-info")
-print(block1)
-
-# https://stackoverflow.com/questions/4531995/getting-attribute-using-xpath
+client = MongoClient('localhost', 27017)
+db = client['mvideo']
+mvideo_db = db.mvideo_db
+mvideo_db.insert_many(items)
