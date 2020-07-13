e14a06082520823f68d654de8060f9470eab9224
alex-kalinichenko <alex@kalinichenko.info>
2020-07-13 20:20:11 +0300

    Parcing HW 8 update

--- parcing/8.scrapy/instaparser/items.py
+++ parcing/8.scrapy/instaparser/items.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-
 # Define here the models for your scraped items
-#
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 
@@ -10,5 +8,7 @@ import scrapy
 
 class InstaparserItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    user_id = scrapy.Field()
+    photo = scrapy.Field()
+    likes = scrapy.Field()
+    post = scrapy.Field()
--- parcing/8.scrapy/instaparser/pipelines.py
+++ parcing/8.scrapy/instaparser/pipelines.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-
 # Define your item pipelines here
-#
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class InstaparserPipeline(object):
+class InstaparserPipeline:
     def process_item(self, item, spider):
+        print(1)
         return item
