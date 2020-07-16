08ddc92ad553fc1dab9edc4a478d3adb8e14026a
alex-kalinichenko <alex@kalinichenko.info>
2020-07-17 02:22:17 +0300

    Update

--- parcing/8.scrapy/instaparser/spiders/instagram.py
+++ parcing/8.scrapy/instaparser/spiders/instagram.py
@@ -14,12 +14,14 @@ class InstagramSpider(scrapy.Spider):
     allowed_domains = ['instagram.com']
     start_urls = ['https://instagram.com/']
     insta_login = 'insta_opt'
-    insta_pwd = '#PWD_INSTAGRAM_BROWSER:10:1594675566:AWlQAGsBrzrbrjzIZ1tbjYwlW0LqXcpKBa+Uc3nXJQPcT08gK1qxDR5qTwdVmUF4hMja/bFtZr6UT0vr+p6cR3ATeYpX86UVA7NeN2+G5aeTjioI42uhldF93K7htGeYmHMAIJCoIe8yDQ=='
+    insta_pwd = '#PWD_INSTAGRAM_BROWSER:10:1594675566:AWlQAGsBrzrbrjzIZ1tbjYwlW0LqXcpKBa+Uc3nXJQPcT08gK1qx' \
+                'DR5qTwdVmUF4hMja/bFtZr6UT0vr+p6cR3ATeYpX86UVA7NeN2+G5aeTjioI42uhldF93K7htGeYmHMAIJCoIe8yDQ=='
     inst_login_link = 'https://www.instagram.com/accounts/login/ajax/'
-    parse_user = 'ai_machine_learning'  # Пользователь, у которого собираем посты. Можно указать список
+    parse_user = 'kmemescluster'  # Пользователь, у которого собираем посты. Можно указать список
 
     graphql_url = 'https://www.instagram.com/graphql/query/?'
-    posts_hash = 'eddbde960fed6bde675388aac39a3657'  # hash для получения данных по постах с главной страницы
+    posts_hash = 'eddbde960fed6bde675388aac39a3657'  # hash для получения данных о постах с главной страницы
+    followers_hash = 'c76146de99bb02f6415203be841dd25a' # hash для получения подписчиков
 
     def parse(self, response: HtmlResponse):  # Первый запрос на стартовую страницу
         csrf_token = self.fetch_csrf_token(response.text)  # csrf token забираем из html
@@ -34,20 +36,21 @@ class InstagramSpider(scrapy.Spider):
     # после авторизации мы попадаем на свою страницу
     def user_parse(self, response: HtmlResponse):
         j_body = json.loads(response.text)  # Преобразуем респонс в джейсон
-        if j_body[
-            'authenticated']:  # Проверяем ответ после авторизации, чтобы понять удачно ли прошла авторизация если есть аут-ия то
+        if j_body['authenticated']:  # Проверяем ответ после авторизации, чтобы понять удачно ли прошла авторизация
             yield response.follow(
                 # переходим на желаемую страницу пользователя. Сделать цикл для кол-ва пользователей больше 2-ух
                 f'/{self.parse_user}',
-                # т.к. скрапи прекрасно работает с отностильными ссылками - этого достаточно для перехода
+                # т.к. скрапи прекрасно работает с относительными ссылками - этого достаточно для перехода
                 callback=self.user_data_parse,  # след. шаг
                 cb_kwargs={'username': self.parse_user}
                 # можем передавать любые значения. Это важно передавать именно так, а не брать из общего
-                # глобального списка потому что мы можем перебирать это в цикле и у вас будут лететь айтемы в абсолютно неопределённом порядке
-                # потому что асинхронно, поэтому надо передавать явно. Поэтому мы передаём этот username в следующий метод user_data_parse явно
+                # глобального списка потому что мы можем перебирать это в цикле и у вас будут лететь айтемы
+                # в абсолютно неопределённом порядке потому что асинхронно, поэтому надо передавать явно.
+                # Поэтому мы передаём этот username в следующий метод user_data_parse явно
             )
 
-    # принимаем username из предыдущего метода user_parse и на руках будет иметь имя пользователя с которым работаем в данном методе
+    # принимаем username из предыдущего метода user_parse и
+    # на руках будет иметь имя пользователя с которым работаем в данном методе
     def user_data_parse(self, response: HtmlResponse, username):
         user_id = self.fetch_user_id(response.text, username)  # Получаем id пользователя методом fetch_user_id
         variables = {'id': user_id,  # Формируем словарь для передачи даных в запрос
@@ -85,9 +88,28 @@ class InstagramSpider(scrapy.Spider):
                 user_id=user_id,
                 photo=post['node']['display_url'],
                 likes=post['node']['edge_media_preview_like']['count'],
-                post=post['node']  # на всякий случай на будущее закидываем всю ноду в post, вдруг понадобятся все данные
+                post=post['node']  # на всякий на будущее закидываем всю ноду в post, вдруг понадобятся данные
             )
         yield item  # В пайплайн
+
+
+
+    def followers_parse(self, response:HtmlResponse, username, variables):
+        j_data = json.loads(response.text)
+        followers_info = j_data.get('data').get('user').get('edge_follower_by')
+        if followers_info.get('has_next_page'): # Если есть следующая страница
+            variables['after'] = followers_info['end_cursor']  # Новый параметр для перехода на след. страницу
+            url_followers = f'{self.graphql_url}query_hash={self.followers_hash}&{urlencode(variables)}'
+            yield response.follow(
+                url_followers,
+                callback=self.followers_parse,
+                cd_kwargs={'followers_name': }
+            )
+        followers = j_data.get('data').get('user').get('edge_owner_to_timeline_media').get('edges')  # Сами подписчики
+        
+
+
+
 
     # Получаем токен для авторизации
     def fetch_csrf_token(self, text):
