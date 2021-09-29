b601868de418c0defc935394592ad2a20318617a
alex-kalinichenko <alex@kalinichenko.info>
2021-09-29 18:23:23 +0300

    move folder kaggle/src back from root to kaggle

--- src/module_reload.py
+++
@@ -1,9 +0,0 @@
-# reload перезагрузка модуля
-from importlib import reload
-reload(src.processing)
-from src.processing import get_num_features
-
-# 2-й способ перезагрузки модуля (reload)
-import os
-os.chdir('C:/Users/Alex/YandexDisk/it/gb/src')
-%run processing.py
