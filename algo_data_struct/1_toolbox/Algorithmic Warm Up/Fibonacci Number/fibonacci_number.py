35b0c7cf1011de6443fee96a56723fc6c1359c9c
alex-kalinichenko <56787559+alex-kalinichenko@users.noreply.github.com>
2022-06-24 00:31:40 +0300

    update

--- algo_data_struct/1_toolbox/Algorithmic Warm Up/Fibonacci Number/fibonacci_number.py
+++ algo_data_struct/1_toolbox/Algorithmic Warm Up/Fibonacci Number/fibonacci_number.py
@@ -15,15 +15,12 @@ def fibonacci_number(n):
 
     if n <= 1:
         return n
-
-    num_0 = 0
-    num_1 = 1
+    num_0, num_1 = 0, 1
     _ = 1
     summ = 0
     while _ < n:
         summ = num_0 + num_1
-        num_0 = num_1
-        num_1 = summ
+        num_0, num_1 = num_1, summ
         _ += 1
     return summ
 
