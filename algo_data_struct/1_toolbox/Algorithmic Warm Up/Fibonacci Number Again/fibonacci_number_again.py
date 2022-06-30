e2d247f178b4475106ec13ffac3d210a359a5c78
alex-kalinichenko <56787559+alex-kalinichenko@users.noreply.github.com>
2022-06-30 21:03:07 +0300

    add solution Fibonacci Number modulo M and Pisano Period

--- algo_data_struct/1_toolbox/Algorithmic Warm Up/Fibonacci Number Again/fibonacci_number_again.py
+++ algo_data_struct/1_toolbox/Algorithmic Warm Up/Fibonacci Number Again/fibonacci_number_again.py
@@ -7,11 +7,19 @@
 # Видите ли вы какие-нибудь интересные свойства двух последних строк в таблице?
 # Обе эти последовательности периодичны! Для m=2 период равен 011 и имеет длину 3,
 # а для m=3 период равен 01120221 и имеет длину 8.
-# Поэтому, чтобы вычислить, скажем, F2015mod3, нужно просто найти остаток от 2015 при делении на 8.
-# Поскольку 2015=251⋅8+7, мы заключаем, что F2015mod3=F7mod3=1.
 # Оказывается, что для любого целого числа m≥2 последовательность Fn mod m периодична.
 # Период всегда начинается с 01 и известен как период Пизано (Пизано - другое имя Фибоначчи).
-
+# Поэтому, чтобы вычислить, скажем, F2015mod3, нужно просто найти остаток от 2015 (порядковый
+# номер числа Фибоначчи) при делении на 8 (модуль).
+# Поскольку 2015=251⋅8+7, мы заключаем, что чтобы найти F2015 по модулю 3 (т.е. остаток от
+# деления числа Фибоначчи номер 2015 на 3) нужно найти остаток от деления на на модуль
+# числа Фибоначчи под номером, равным длине периода Пизано т.е.: F2015mod3=F7mod3=1.
+# Или:
+# 1. находим длину последовательности Пизано для модуля m
+# 2. находим остаток от деления номера числа Фибоначчи на длину посл-сти Пизано для m
+# 3. находим остаток от деления числа Фибоначчи с номером остатка из п.2 на модуль m
+# Это и будет ответом.
+# https://www.geeksforgeeks.org/fibonacci-number-modulo-m-and-pisano-period/
 
 def fibonacci_number_again_naive(n, m):
     assert 0 <= n <= 10 ** 18 and 2 <= m <= 10 ** 3
@@ -29,40 +37,24 @@ def fibonacci_number_again_naive(n, m):
 def fibonacci_number_again(n, m):
     assert 0 <= n <= 10 ** 18 and 2 <= m <= 10 ** 3
 
+    def pizano_period(m):
+        previous, current = 0, 1
+        for i in range(1, m * m):
+            previous, current = current, (previous + current) % m
+            if previous == 0 and current == 1:
+                return i
+
     def fibo(n):
-        """ Find n-th Fibonacci number """
         if n <= 1:
             return n
-        num_0, num_1 = 0, 1
-        _ = 1
-        summ = 0
-        while _ < n:
-            summ = num_0 + num_1
-            num_0, num_1 = num_1, summ
-            _ += 1
-        return summ
+        else:
+            previous, current = 0, 1
+            for i in range(n):
+                previous, current = current, previous + current
+            # print(n, previous)
+            return previous
 
-    def pizano_seq_len(n, m):
-        """
-        Calculate length of Pizano sequence
-        n: number of Fibo
-        mod: modulo
-        :return: length of Pizano sequence
-        """
-        pizano_seq = [0, 1] # every Pizano sequens starts from 01
-        for i in range(2, n): # start to add elements from 3-rd
-            f = fibo(i)
-            # print('fibo=', f, 'n=', n)
-            # print('reminder=', fibo(i) % mod)
-            pizano_seq.append(fibo(i) % m) # append reminder from F_n / mod
-            # print('pizano_seq=', pizano_seq)
-            if pizano_seq[-2:] == [0, 1]: # stop when 01 meet in the Pizano sequence again
-                # print('pizano_seq=', pizano_seq)
-                # print(len(pizano_seq))
-                return len(pizano_seq) - 2
-    # print(f'n={n}, F_n={fibo(n)}')
-    # print(f'Pizano seq len={pizano_seq_len(n, m)}')
-    reminder = n % pizano_seq_len(n, m)
+    reminder = n % pizano_period(m)
     return fibo(reminder) % m
 
 
