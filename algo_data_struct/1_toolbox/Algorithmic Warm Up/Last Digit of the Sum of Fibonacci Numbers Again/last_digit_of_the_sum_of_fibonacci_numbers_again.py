f1caf0c2fa17fbad35d2cea8e0a78f5023e97fdd
alex-kalinichenko <56787559+alex-kalinichenko@users.noreply.github.com>
2022-07-14 12:10:54 +0300

    update

--- algo_data_struct/1_toolbox/Algorithmic Warm Up/Last Digit of the Sum of Fibonacci Numbers Again/last_digit_of_the_sum_of_fibonacci_numbers_again.py
+++ algo_data_struct/1_toolbox/Algorithmic Warm Up/Last Digit of the Sum of Fibonacci Numbers Again/last_digit_of_the_sum_of_fibonacci_numbers_again.py
@@ -2,7 +2,7 @@
 
 
 def last_digit_of_the_sum_of_fibonacci_numbers_again_naive(from_index, to_index):
-    assert 0 <= from_index <= to_index <= 10 ** 18
+    assert 0 <= from_index <= to_index <= 10**18
 
     if to_index == 0:
         return 0
@@ -19,7 +19,17 @@ def last_digit_of_the_sum_of_fibonacci_numbers_again_naive(from_index, to_index)
 def last_digit_of_the_sum_of_fibonacci_numbers_again(from_index, to_index):
     assert 0 <= from_index <= to_index <= 10 ** 18
 
-    type here
+    if to_index <= 1:
+        return to_index
+    else:
+        previous, current = 0, 1
+        summ = 1
+        for i in range(2, to_index + 1):
+            previous, current = current, previous + current
+            if i >= from_index:
+                summ += current
+                #print(f'i={i} current={current}, summ={summ}')
+        return summ % 10 if from_index <= 1 else (summ - 1) % 10
 
 
 if __name__ == '__main__':
